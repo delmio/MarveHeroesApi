ee84865c35a855dede89aefa68810602202cb98c
Felipe Del Rio <felipeandresdelriovera@gmail.com>
2022-01-21 18:21:16 -0300

    aginador con offsert

--- src/controller/characters.js
+++ src/controller/characters.js
@@ -24,6 +24,29 @@ async function characters(req,res) {
     
 }
 
+async function charactersWithOffset(req,res) {
+     
+    let url = env.baseurl + "characters?ts=1&apikey=" + env.publicKey_ +  "&hash=" + env.marvelHasher() + '&offset=' + req.params.offset;
+   
+    let headers = {
+        "headers":{
+            "content-type": "application/json"
+        }
+    };
+
+    try {
+ 
+        await get(url,headers)
+        .then((response)=>{
+            res.status(200).send(response.data); 
+        });  
+        
+    } catch (error) {
+        res.status(500).send(error);
+    } 
+    
+}
+
 async function charactersById(req,res){
 
     let url = env.baseurl + "characters/"+req.params.id+"?ts=1&apikey=" + env.publicKey_ +  "&hash=" + env.marvelHasher();
@@ -50,5 +73,6 @@ async function charactersById(req,res){
  
 module.exports = {
     characters,
-    charactersById
+    charactersById,
+    charactersWithOffset
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 var characters = require('../controller/characters');
 
 router.get('/characters/', characters.characters);
+router.get('/characters/offset/:offset', characters.charactersWithOffset);
 router.get('/characters/:id', characters.charactersById);
 
 module.exports = router;
